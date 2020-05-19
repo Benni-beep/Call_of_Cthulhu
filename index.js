652e514ee8c11791f0c81ce6ce729c0fbbce10d4
Benni-beep <krikelkr4kel@gmail.com>
2020-05-19 07:35:04 +0200

    first label, input and Eventlistener examples

---
+++ index.js
@@ -0,0 +1,10 @@
+  // nameInput.addEventListener("blur", function() {console.log('blur')});
+  // nameInput.addEventListener("click", function() {console.log('click')});
+  // nameInput.addEventListener("focus", function() {console.log('focus')});
+  // nameInput.addEventListener("input", function() {console.log('input')});
+  // document.addEventListener("keyup", console.log);
+  document.addEventListener("keydown", function(event) {
+    // event.preventDefault();
+    console.log(event.key);
+  });
+  // document.addEventListener("keypress", console.log);
